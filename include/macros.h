88f4383d42e087bae157ce9be090bfbf2abe3c06
RayhaanT <tanweer.rayhaan@gmail.com>
2021-04-02 16:43:58 -0400

    Cleaned up macros

--- include/macros.h
+++ include/macros.h
@@ -2,23 +2,11 @@
 
 #include "main.h"
 
-void stackCubes();
-void disengageStack();
-void dropTray();
-void halfStack();
-void liftToMid();
-void liftToLow();
-void dropLift();
 void flipout();
-void incrementLift(int dir);
-
 void in();
 void stopRollers();
 void shootClean();
 void shootStaggered();
 void shootStaggeredIntake();
 
-extern bool trayUp;
-extern bool stacking;
-extern bool lifting;
 extern bool suspendDrive;
